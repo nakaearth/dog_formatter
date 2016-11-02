28cd56561a10b33ec348e5f1835f67a4e0e85248
nakamura shinichirou <naka5313@gmail.com>
2016-11-03 00:00:04 +0900

    テスト追加

--- lib/dog_formatter/model.rb
+++ lib/dog_formatter/model.rb
@@ -5,17 +5,17 @@ module DogFormatter
    included do
    end
 
-    module ClassMethods
-      def method_missing(method_name, *_args, &_block)
-        return unless method_name.to_s.end_with?("_ymd")
+    def method_missing(method_name, *_args, &_block)
+      return unless method_name.to_s.end_with?("_ymd")
 
-        method_name_except_format_value = method_name.to_s.sub!(/_ymd/, '')
-        column_names = self.class.column_names.select { |column_name| column_name =~ /.*_at/ }
+      method_name_except_format_value = method_name.to_s.sub!(/_ymd/, '')
+      column_names = self.class.column_names.select { |column_name| column_name =~ /.*_at/ }
 
-        return unless column_names.include?(method_name_except_format_value)
+      return unless column_names.include?(method_name_except_format_value)
 
-        read_attribute(method_name_except_format_value).strftime("%Y/%m/%d")
-      end
+      read_attribute(method_name_except_format_value).strftime("%Y/%m/%d")
     end
   end
 end
+
+ActiveRecord::Base.send :include, DogFormatter::Model
--- test/dog_formatter_test.rb
+++ test/dog_formatter_test.rb
@@ -2,11 +2,14 @@ require 'test_helper'
 require 'dog_formatter/model'
 
 class User < ActiveRecord::Base
-  include DagFormatter::Model
 end
 
 class DogFormatterTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_equal Date.new.strftime("%Y/%m/%d"), User.create(name: 'ほげ').created_at_ymd
+  test "#created_at_ymd" do
+    assert_equal Date.current.strftime("%Y/%m/%d"), User.create(name: 'ほげ').created_at_ymd
+  end
+
+  test "#updated_at_ymd" do
+    assert_equal Date.current.strftime("%Y/%m/%d"), User.create(name: 'ほげ').updated_at_ymd
   end
 end
