aabbe019aad0eb7165ba1fce5728d2986c723954
nakaearth <naka5313@gmail.com>
2016-11-04 12:31:44 +0900

    テスト追加。コードのリファクタ

--- lib/dog_formatter/model.rb
+++ lib/dog_formatter/model.rb
@@ -6,14 +6,52 @@ module DogFormatter
    end
 
     def method_missing(method_name, *_args, &_block)
-      return unless method_name.to_s.end_with?("_ymd")
+      method_name_suffix =  matched_method_name_suffix(method_name.to_s)
+      return unless method_name_suffix
 
-      method_name_except_format_value = method_name.to_s.sub!(/_ymd/, '')
-      column_names = self.class.column_names.select { |column_name| column_name =~ /.*_at/ }
+      suffix_pattern = Regexp.new(method_name_suffix)
+      attribute_name_except_format_value = method_name.to_s.sub!(suffix_pattern, '')
 
-      return unless column_names.include?(method_name_except_format_value)
+      return unless exist_column?(attribute_name_except_format_value)
 
-      read_attribute(method_name_except_format_value).strftime("%Y/%m/%d")
+      formatted_with_method_name_suffix(attribute_name_except_format_value, method_name_suffix)
+    end
+
+    private
+    def format_suffixs
+      ['_ymd', '_ymd_ja', '_ym', '_ym_ja']
+    end
+
+    def matched_method_name_suffix(method_name)
+      format_suffixs.each { |suffix| return suffix if method_name.end_with?(suffix) }
+    end
+
+    def date_columns
+      self.class.column_names.select { |column_name| column_name =~ /.*_at/ }
+    end
+
+    def exist_column?(attribute_name)
+      date_columns.include?(attribute_name)
+    end
+
+    def formatted_with_method_name_suffix(attribute_name, method_name_suffix)
+      read_attribute(attribute_name).strftime(format_text(method_name_suffix))
+    end
+
+    def format_text(method_name_suffix)
+      # TODO: 設定ファイルなどに外出ししたものを読み込んでマッチした形式を使うようにする
+      case method_name_suffix
+      when '_ymd'
+        '%Y/%m/%d'
+      when '_ymd_ja'
+        '%Y年%m月%d日'
+      when '_ym'
+        '%Y/%m'
+      when '_ym_ja'
+        '%Y年%m月'
+      else
+        raise '変換できません。該当する日付形式がありません'
+      end
     end
   end
 end
--- test/dog_formatter_test.rb
+++ test/dog_formatter_test.rb
@@ -12,4 +12,22 @@ class DogFormatterTest < ActiveSupport::TestCase
   test "#updated_at_ymd" do
     assert_equal Date.current.strftime("%Y/%m/%d"), User.create(name: 'ほげ').updated_at_ymd
   end
+
+  test "#created_at_ymd_ja" do
+    assert_equal Date.current.strftime("%Y年%m月%d日"), User.create(name: 'ほげ').created_at_ymd_ja
+  end
+
+  test "#created_at_ym" do
+    assert_equal Date.current.strftime("%Y/%m"), User.create(name: 'ほげ').created_at_ym
+  end
+
+  test "#created_at_ym_ja" do
+    assert_equal Date.current.strftime("%Y年%m月"), User.create(name: 'ほげ').created_at_ym_ja
+  end
+
+  test "#created_at_ymdhhhhh" do
+    # assert_raises('変換できません。該当する日付形式がありません') do
+    #   User.create(name: 'ほげ').created_at_ym_ja
+    # end
+  end
 end
